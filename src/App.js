ea93a5b692f52416130f09a313f4694c4d2b4b79
Oliver <2205456534@qq.com>
2019-05-02 21:30:50 +0800

    JSX语法练习

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+ const Button = props => {
+  const { kind, ...other } = props;
+  const className = kind === "primary" ? "PrimaryButton" : "SecondaryButton";
+  console.log(props);
+  console.log(other);
+  return <button className={className} {...other} />;
+};
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button kind="primary" onClick={() => console.log("clicked!")}>
+            Hello React!
+      </Button>
     </div>
   );
-}
+};
 
 export default App;
